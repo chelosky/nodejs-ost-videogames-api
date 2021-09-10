bf364ca86616ce57f2df97cd439337f62e4bd4b8
Chelosky <marcelolazo19972013@hotmail.com>
2021-09-10 02:42:10 -0300

    TDD

--- src/config.js
+++ src/config.js
@@ -3,10 +3,13 @@ config();
 
 const {DATA_BASE_NAME_PROD, DATA_BASE_NAME_TEST, DATA_BASE_NAME_DEV, NODE_ENV} = process.env;
 
-const DATA_BASE = NODE_ENV === 'test' 
-    ? DATA_BASE_NAME_TEST
-    : DATA_BASE_NAME_DEV
-;
+const DB_ENV = {
+    test: DATA_BASE_NAME_TEST,
+    production: DATA_BASE_NAME_PROD,
+    development: DATA_BASE_NAME_DEV
+}
+
+const DATA_BASE = DB_ENV[NODE_ENV];
 
 /** PARA LA VARIABLES DE ENTORNO */
 export default {
